d2ff07d6cacd1c8ab5056e4c5af6ef83d5825879
Kavinda Gayashan <Kavinda@CreativeSoftware.com>
2020-02-06 20:37:30 +0530

    wip implement the sms reader service

---
+++ AlumniSms/AlumniSms.Android/Services/ReadSmsService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlumniSms.Services;
+using Android.Content;
+using Android.Net;
+
+namespace AlumniSms.Droid.Services
+{
+    public class ReadSmsService : IReadSmsService
+    {
+        private readonly Context _context;
+
+        public ReadSmsService(Context context)
+        {
+            _context = context;
+        }
+
+        const string Inbox = "content://sms/inbox";
+
+        public Task<IEnumerable<ReceivedSms>> ReadSms(string startTag)
+        {
+            return Task.Run(() =>
+            {
+                var smsList = new List<ReceivedSms>();
+                var cursor = _context.ContentResolver.Query(Uri.Parse(Inbox), null, null, null, null);
+
+                if (cursor.MoveToFirst())
+                {
+                    // must check the result to prevent exception
+                    do
+                    {
+                        string msgData = "";
+                        for (int idx = 0; idx < cursor.ColumnCount; idx++)
+                        {
+                            msgData += " " + cursor.GetColumnName(idx) + ":" + cursor.GetString(idx);
+                        }
+
+                        smsList.Add(new ReceivedSms {Sender = "", Text = msgData});
+                    } while (cursor.MoveToNext());
+                }
+
+                return smsList.AsEnumerable();
+            });
+        }
+    }
+}
---
+++ AlumniSms/AlumniSms/Services/IReadSmsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlumniSms.Services
+{
+    public interface IReadSmsService
+    {
+        Task<IEnumerable<ReceivedSms>> ReadSms(string startTag);
+    }
+
+    public class ReceivedSms
+    {
+        public string Sender { get; set; }
+        public string Text { get; set; }
+    }
+}

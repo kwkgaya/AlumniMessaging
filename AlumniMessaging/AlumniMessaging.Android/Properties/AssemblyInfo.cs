1436c3eb14cf6e55e7f52f26bbffeca251559ea0
Kavinda Gayashan <Kavinda@CreativeSoftware.com>
2020-02-09 13:28:14 +0530

    Configure the release build

--- AlumniMessaging/AlumniMessaging.Android/Properties/AssemblyInfo.cs
+++ AlumniMessaging/AlumniMessaging.Android/Properties/AssemblyInfo.cs
@@ -32,3 +32,12 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadSms)]
+[assembly: UsesPermission(Android.Manifest.Permission.SendSms)]
+
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
